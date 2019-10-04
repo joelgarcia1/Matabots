a363e46caec8bf96984c8f136a44037d5bad2074
Joel Garcia Valencia <joel.garciavalencia.372@my.csun.edu>
2019-10-03 18:07:18 -0700

    update

--- 2018-2019 Turning Point/VCS/v5/Miscellaneous/Member_Specific/Pablo/J_Comp_In_Case/include/print_out_data.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PRINT_OUT_DATA_HPP
-#define PRINT_OUT_DATA_HPP
-#include "main.h"
-
-extern void print_out_flywheel(void* pntr);
-extern void print_out();
-
-#endif
--- 2018-2019 Turning Point/VCS/v5/Miscellaneous/Member_Specific/Pablo/J_Comp_In_Case/run&Compile.sh
+++
@@ -1,7 +0,0 @@
-prosv5 make
-echo "Uploading..."
-echo "Uploading..."
-echo "Uploading..."
-{
-prosv5 upload
-} &> /dev/null
